4103c8724031466e8c1cf228ff9da89528fad917
Zakaria Banid <162604734+banidzakaria101@users.noreply.github.com>
2026-09-09 16:28:59 +0100

    fix: styling problem

--- movie-app-rec/next.config.ts
+++ movie-app-rec/next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["m.media-amazon.com"],
   },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
--- movie-app-rec/pages/_app.tsx
+++ movie-app-rec/pages/_app.tsx
@@ -3,6 +3,9 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useRouter } from 'next/router';
+import "../styles/globals.css";
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
